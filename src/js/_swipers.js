f67f998eae4a7304d68a465bd6b8081eaf8343db
Anna-Anka <anjbekrenewa@gmail.com>
2023-03-24 14:29:22 +0300

    added other-models

--- src/js/_swipers.js
+++ src/js/_swipers.js
@@ -1,3 +1,6 @@
+import { otherModelsSwiper } from "./swipers/_other-models";
 import { productCardSlider } from "./swipers/_product-card-slider";
 
 productCardSlider();
+
+otherModelsSwiper();
---
+++ src/js/swipers/_other-models.js
@@ -0,0 +1,51 @@
+import './../_libs'
+
+export const otherModelsSwiper = () => {
+    if (document.querySelector('.other-models__swiper')) {
+        return new Swiper('.other-models__swiper', {
+            loop: true,
+            speed: 800,
+
+            navigation: {
+                nextEl: '.other-models__button--next',
+                prevEl: '.other-models__button--prev',
+            },
+
+            pagination: {
+                el: '.other-models__pagination',
+                clickable: true,
+            },
+
+            scrollbar: {
+                el: ".other-models__scrollbar",
+                draggable: true
+            },
+
+            breakpoints: {
+                1100: {
+                    slidesPerGroup: 3,
+                    slidesPerView: 3,
+                    spaceBetween: 26,
+                },
+
+                768: {
+                    slidesPerGroup: 4,
+                    slidesPerView: 4.6,
+                    spaceBetween: 10,
+                },
+
+                560: {
+                    slidesPerGroup: 3,
+                    slidesPerView: 3.2,
+                    spaceBetween: 10,
+                },
+
+                320: {
+                    slidesPerGroup: 2,
+                    slidesPerView: 2.2,
+                    spaceBetween: 10,
+                },
+            },
+        });
+    }
+};
