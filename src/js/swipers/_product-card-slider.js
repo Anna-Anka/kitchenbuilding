44530c4cdcbb282a41a6880cc0f0253f19a9a2b6
Anna-Anka <anjbekrenewa@gmail.com>
2023-08-18 12:41:14 +0300

    added portfolio-page, corrected product-card-slider.js

--- src/js/swipers/_product-card-slider.js
+++ src/js/swipers/_product-card-slider.js
@@ -11,49 +11,53 @@ if (document.querySelector('.product-card__main')) {
         const sliderMain = card.querySelector('.slider-main');
         const sliderNav = card.querySelector('.slider-nav');
 
-        const swiperSmall = new Swiper(sliderNav, {
-            slidesPerView: 9,
-            spaceBetween: 10,
-            loopedSlides: 9,
-            freeMode: true,
-        });
+        const items = card.querySelectorAll('.product-card__big')
+        if (items.length > 1) {
+            const swiperSmall = new Swiper(sliderNav, {
+                slidesPerView: 9,
+                spaceBetween: 10,
+                loopedSlides: 9,
+                freeMode: true,
+            });
 
-        const swiperMain = new Swiper(sliderMain, {
-            spaceBetween: 10,
-            loopedSlides: 9,
-            speed: 500,
+            const swiperMain = new Swiper(sliderMain, {
+                spaceBetween: 10,
+                loopedSlides: 9,
+                speed: 500,
 
-            thumbs: {
-                swiper: swiperSmall,
-            },
+                thumbs: {
+                    swiper: swiperSmall,
+                },
 
-            navigation: {
-                nextEl: card.querySelector('.product-card__button--next'),
-                prevEl: card.querySelector('.product-card__button--prev')
-            },
+                navigation: {
+                    nextEl: card.querySelector('.product-card__button--next'),
+                    prevEl: card.querySelector('.product-card__button--prev')
+                },
 
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-            },
-        })
-
-        const changeSlide = () => {
-            const slideActive = swiperMain.realIndex + 1
-
-            const priceAll = card.querySelectorAll('[data-price]')
-
-            priceAll.forEach(price => {
-                const valuePrice = Number(price.getAttribute('data-price'))
-                if (valuePrice === slideActive) {
-                    price.classList.add('product-card__price--active')
-                } else {
-                    price.classList.remove('product-card__price--active')
-                }
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                },
             })
-        }
 
-        swiperMain.init(changeSlide())
-        swiperMain.on('slideChange', changeSlide)
+            const changeSlide = () => {
+                const slideActive = swiperMain.realIndex + 1
+
+                const priceAll = card.querySelectorAll('[data-price]')
+                console.log(priceAll)
+
+                priceAll.forEach(price => {
+                    const valuePrice = Number(price.getAttribute('data-price'))
+                    if (valuePrice === slideActive) {
+                        price.classList.add('product-card__price--active')
+                    } else {
+                        price.classList.remove('product-card__price--active')
+                    }
+                })
+            }
+
+            swiperMain.init(changeSlide())
+            swiperMain.on('slideChange', changeSlide)
+        }
     })
 };
