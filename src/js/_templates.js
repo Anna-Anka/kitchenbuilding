c208e58e20e351f58292ac7f69f03efd1188ac67
Anna-Anka <anjbekrenewa@gmail.com>
2023-08-24 16:54:33 +0300

    added modal-thank

--- src/js/_templates.js
+++ src/js/_templates.js
@@ -34,6 +34,16 @@ smoothScroll();
 import GraphModal from 'graph-modal';
 const modal = new GraphModal('modal');
 
+if (document.querySelector('.modal-thank')) {
+    const btnThank = document.querySelector('.modal-thank')
+
+    btnThank.addEventListener('click', () => {
+        const modal = new GraphModal('modal').open('modal-thank');
+    });
+
+    // btnThank.click();
+}
+
 import mixitup from 'mixitup';
 
 if (document.querySelector('.articles-block')) {
