8cd264e2d1b1392dc5f90001f9c533d7bad7d4ee
Anna-Anka <anjbekrenewa@gmail.com>
2023-08-30 18:47:03 +0300

    corrected some moments

--- src/js/_libs.js
+++ src/js/_libs.js
@@ -2,13 +2,3 @@ import './libs/_tabs';
 import './libs/_modals';
 import './libs/_inputmask';
 import './libs/_mixitup';
-
-
-// * Swiper
-//import '../../node_modules/swiper/swiper-bundle.min.js'
-
-// * rangeslider
-//import '../../node_modules/ion-rangeslider/js/ion.rangeSlider.js'
-
-// * nounslider (price filter vanila js)
-//import '../../node_modules/nouislider/dist/nouislider.min.js'
